8e7602d08b9ddb7c2279ea67836064758a7ba058
Cracked5pider <cracked5pider@protonmail.com>
2022-09-17 00:40:06 +0200

    remove StringCompareW function

--- Teamserver/data/implants/Demon/Source/Core/Transport.c
+++ Teamserver/data/implants/Demon/Source/Core/Transport.c
@@ -195,17 +195,6 @@ BOOL TransportInit( PPACKAGE Package )
     return Success;
 }
 
-INT StringCompareW( _In_ LPCWSTR String1, _In_ LPCWSTR String2 )
-{
-    for (; *String1 == *String2; String1++, String2++)
-    {
-        if (*String1 == '\0')
-            return 0;
-    }
-
-    return ((*(LPCWSTR)String1 < *(LPCWSTR)String2) ? -1 : +1);
-}
-
 BOOL TransportSend( LPVOID Data, SIZE_T Size, PVOID* RecvData, PSIZE_T RecvSize )
 {
 #ifdef TRANSPORT_HTTP
