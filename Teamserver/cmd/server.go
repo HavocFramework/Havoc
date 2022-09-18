07d73555b9c55ea0d505861b78fc5b86560cc666
Cracked5pider <cracked5pider@protonmail.com>
2022-09-18 16:39:26 +0200

    removed debug function

--- Teamserver/cmd/server.go
+++ Teamserver/cmd/server.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-    "io"
-    "log"
     "os"
 
     "Havoc/pkg/logger"
@@ -29,46 +27,6 @@ func init() {
     ServerCli.Flags().BoolVarP(&teamserverFlags.Server.Verbose, "verbose", "v", false, "verbose messages")
     ServerCli.Flags().BoolVarP(&teamserverFlags.Server.Debug, "debug", "d", false, "enable debug mode")
     ServerCli.Flags().BoolVarP(&teamserverFlags.Server.DebugDev, "debug-dev", "", false, "enable debug mode for developers (compiles the agent with the debug mode/macro enabled)")
-}
-
-func logOutput() func() {
-    logfile := `logfile`
-    // open file read/write | create if not exist | clear file at open if exists
-    f, _ := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
-
-    // save existing stdout | MultiWriter writes to saved stdout and file
-    out := os.Stdout
-    mw := io.MultiWriter(out, f)
-
-    // get pipe reader and writer | writes to pipe writer come out pipe reader
-    r, w, _ := os.Pipe()
-
-    // replace stdout,stderr with pipe writer | all writes to stdout, stderr will go through pipe instead (fmt.print, log)
-    os.Stdout = w
-    os.Stderr = w
-
-    // writes with log.Print should also write to mw
-    log.SetOutput(mw)
-
-    //create channel to control exit | will block until all copies are finished
-    exit := make(chan bool)
-
-    go func() {
-        // copy all reads from pipe to multiwriter, which writes to stdout and file
-        _, _ = io.Copy(mw, r)
-        // when r or w is closed copy will finish and true will be sent to channel
-        exit <- true
-    }()
-
-    // function to be deferred in main until program exits
-    return func() {
-        // close writer then block on exit channel | this will let mw finish writing before the program exits
-        _ = w.Close()
-        <-exit
-        // close file after all writes have finished
-        _ = f.Close()
-    }
-
 }
 
 func serverFunc(cmd *cobra.Command, args []string) error {
