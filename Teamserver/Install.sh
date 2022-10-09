79601490c3a50baa07b2bf69cff3e542b234f685
Cracked5pider <cracked5pider@protonmail.com>
2022-10-09 21:02:06 +0200

    updated installation of musl.cc cross compile for agent generation.

--- Teamserver/Install.sh
+++ Teamserver/Install.sh
@@ -1,3 +1,6 @@
 echo "[*] Install libs for the teamserver..."
 
-sudo apt --yes install golang-go nasm mingw-w64
+sudo apt --yes install golang-go nasm mingw-w64 wget 
+
+wget https://musl.cc/x86_64-w64-mingw32-cross.tgz -O /tmp/mingw-musl.tgz 
+tar zxvf /tmp/mingw-musl.tgz -C data
