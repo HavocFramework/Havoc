2ecf4b58adc682641bcb04897da26214c0d18907
Cracked5pider <cracked5pider@protonmail.com>
2023-04-25 13:07:16 +0200

    refactoring client db code. removing legacy code and unused functions. more verbose error handling using spdlog instead of cout

--- client/Source/Havoc/DBManger/DBManager.cpp
+++ client/Source/Havoc/DBManger/DBManager.cpp
@@ -37,92 +37,37 @@ DBManager::DBManager( const QString& FilePath, int OpenFlag )
 
 bool DBManager::createNewDatabase()
 {
-    bool success = false;
-    if ( DB.isOpen() )
-    {
-        auto query = QSqlQuery();
+    auto query = QSqlQuery();
 
-        // Teamserver
-        query.prepare(
-                "CREATE TABLE \"Teamservers\" ( "
-                "\"ID\" INTEGER PRIMARY KEY, "
-                "\"ProfileName\" TEXT, "
-                "\"Host\" TEXT, "
-                "\"Port\" INTEGER, "
-                "\"User\" TEXT, "
-                "\"Password\" TEXT "
-                ");"
-        );
-        success = query.exec();
-        if ( ! success )
-            spdlog::error( "[DB] Couldn't create Teamserver table: ", query.lastError().text().toStdString() );
-
-        // Sessions
-        query.prepare(
-                "CREATE TABLE \"Sessions\" ( "
-                "\"ID\" INTEGER PRIMARY KEY, "
-                "\"TeamserverID\" INTEGER, "
-                "\"NameID\" TEXT, "
-                "\"External\" TEXT, "
-                "\"Internal\" TEXT, "
-                "\"Listener\" TEXT, "
-                "\"User\" TEXT, "
-                "\"Computer\" TEXT, "
-                "\"OS\" TEXT, "
-                "\"Process\" TEXT, "
-                "\"PID\" INTEGER, "
-                "\"Arch\" TEXT, "
-                "\"Last\" TEXT"
-                ");"
-        );
-        success = query.exec();
-        if ( ! success )
-            spdlog::error( "[DB] Couldn't create Sessions table: ", query.lastError().text().toStdString() );
-
-        // Listener
-        query.prepare(
-                "CREATE TABLE \"Listeners\" ( "
-                "\"ID\" INTEGER PRIMARY KEY, "
-                "\"TeamserverID\" INTEGER, "
-                "\"Name\" TEXT, "
-                "\"Protocol\" TEXT, "
-                "\"Host\" TEXT, "
-                "\"Port\" INTEGER, "
-                "\"Connected\" INTEGER, "
-                "\"Status\" TEXT"
-                ");"
-        );
-        success = query.exec();
-        if ( ! success )
-            spdlog::error( "[DB] Couldn't create Listener table: ", query.lastError().text().toStdString() );
-
-        // Credentials
-        query.prepare(
-                "CREATE TABLE \"Credentials\" ( "
-                "\"ID\" INTEGER PRIMARY KEY, "
-                "\"TeamserverID\" INTEGER, "
-                "\"User\" TEXT, "
-                "\"Password\" TEXT, "
-                "\"Type\" TEXT, "
-                "\"Domain\" TEXT, "
-                "\"Source\" TEXT, "
-                "\"Added\" TEXT "
-                ");"
-        );
-        success = query.exec();
-        if ( ! success )
-            spdlog::error( "[DB] Couldn't create Credentials table: ", query.lastError().text().toStdString() );
-
-        // Scripts
-        query.prepare(
-                "CREATE TABLE \"Scripts\" ( "
-                "\"ID\" INTEGER PRIMARY KEY, "
-                "\"Path\" TEXT "
-                ");"
-        );
-        success = query.exec();
-        if ( ! success )
-            spdlog::error( "[DB] Couldn't create Scripts table: ", query.lastError().text().toStdString() );
+    /* check if the db file is opened */
+    if ( ! DB.isOpen() ) {
+        return false;
     }
-    return success;
+
+    query.prepare(
+        "CREATE TABLE \"Teamservers\" ( "
+        "\"ID\" INTEGER PRIMARY KEY, "
+        "\"ProfileName\" TEXT, "
+        "\"Host\" TEXT, "
+        "\"Port\" INTEGER, "
+        "\"User\" TEXT, "
+        "\"Password\" TEXT "
+        ");"
+    );
+    if ( ! query.exec() ) {
+        spdlog::error( "[DB] Couldn't create Teamserver table: ", query.lastError().text().toStdString() );
+    }
+
+    query.prepare(
+        "CREATE TABLE \"Scripts\" ( "
+        "\"ID\" INTEGER PRIMARY KEY, "
+        "\"Path\" TEXT "
+        ");"
+    );
+    if ( ! query.exec() ) {
+        spdlog::error( "[DB] Couldn't create Scripts table: ", query.lastError().text().toStdString() );
+        return false;
+    }
+
+    return true;
 }
