7bff63633b6889a9f9dcfb498fdb00529e7a9d90
Steve S <santos.s.salinas@gmail.com>
2023-10-09 14:18:49 -0700

    Update handlers.go
    Added callback time updates to 3rd party agents

--- teamserver/pkg/handlers/handlers.go
+++ teamserver/pkg/handlers/handlers.go
@@ -327,7 +327,12 @@ func handleServiceAgent(Teamserver agent.TeamServer, Header agent.Header, Extern
 	if Agent != nil {
 		AgentData = Agent.ToMap()
 	}
-
+	
+	// Update Callback time - 0xtriboulet
+	if Teamserver.AgentExist(Header.AgentID) {
+		Agent.UpdateLastCallback(Teamserver)
+	}
+	
 	Task = Teamserver.ServiceAgent(Header.MagicValue).SendResponse(AgentData, Header)
 	//logger.Debug("Response:\n", hex.Dump(Task))
 
