4bff9613fc78e0538a3e44976d5bf07c3cefbd09
Cracked5pider <cracked5pider@protonmail.com>
2023-11-06 10:25:18 +0100

    update havoc version number and name. Havoc 0.7 [Bites the Dust]

--- client/Source/global.cpp
+++ client/Source/global.cpp
@@ -9,8 +9,8 @@ using namespace std;
 using namespace HavocNamespace;
 using namespace HavocNamespace::HavocSpace;
 
-string HavocNamespace::Version  = "0.6";
-string HavocNamespace::CodeName = "Hierophant Green";
+string HavocNamespace::Version  = "0.7";
+string HavocNamespace::CodeName = "Bites The Dust";
 
 // Global Variables in the Havoc Namespace
 HavocSpace::Havoc* HavocNamespace::HavocApplication;
