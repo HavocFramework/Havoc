f4ae1d5bc42e0c182df26d9eb618762fb66ccf47
DeathNet123 <arslan.computer2002@gmail.com>
2024-02-22 17:53:27 +0500

    On branch arslan-director-dhavoc Changes to be committed:
    modified:   makefile
    Remove the auto branch fetching for the client modules cloning to literal main just cuz
    of the branch issues
    
    modified:   teamserver/Install.sh
    Commented the install.sh because it was downloading the compilers
    everytime after reboot because of the storage in tmp
    
    modified:   teamserver/cmd/server/teamserver.go
    
    1- Added the function for the Teamserver object to convert it into the
    TeamserverJson
    2- Added the GET endpoint on the teamserver engine called quick to dump
    the TeamserverJson
    
    modified:   teamserver/cmd/server/types.go
    1- Added the TeamserverJson struct
    2- Added the ClientJson struct

--- teamserver/Install.sh
+++ teamserver/Install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if [ ! -d "dir/x86_64-w64-mingw32-cross" ]; then
-	sudo apt -qq --yes install golang-go nasm mingw-w64 wget >/dev/null 2>&1
+# if [ ! -d "dir/x86_64-w64-mingw32-cross" ]; then
+# 	sudo apt -qq --yes install golang-go nasm mingw-w64 wget >/dev/null 2>&1
 
-	if [ ! -d "data" ]; then
-		mkdir data
-	fi
+# 	if [ ! -d "data" ]; then
+# 		mkdir data
+# 	fi
 
-	if [ ! -f /tmp/mingw-musl-64.tgz ]; then
-		wget https://musl.cc/x86_64-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-64.tgz
-	fi
+# 	if [ ! -f /tmp/mingw-musl-64.tgz ]; then
+# 		wget https://musl.cc/x86_64-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-64.tgz
+# 	fi
 
 
-	tar zxf /tmp/mingw-musl-64.tgz -C data
+# 	tar zxf /tmp/mingw-musl-64.tgz -C data
 
-	if [ ! -f /tmp/mingw-musl-32.tgz ]; then
-		wget https://musl.cc/i686-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-32.tgz
-	fi
+# 	if [ ! -f /tmp/mingw-musl-32.tgz ]; then
+# 		wget https://musl.cc/i686-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-32.tgz
+# 	fi
 
-	tar zxf /tmp/mingw-musl-32.tgz -C data
-fi
+# 	tar zxf /tmp/mingw-musl-32.tgz -C data
+# fi
